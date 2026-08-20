97cdc00a21204b421cdb4c1711ee32aa92e58b7b
allie-delehanty <allie.delehanty@gmail.com>
2026-08-20 09:30:05 -0400

    image thing

--- industry-verticals/retail/next.config.js
+++ industry-verticals/retail/next.config.js
@@ -47,6 +47,11 @@ const nextConfig = {
         hostname: 'placehold.co',
         port: '',
       },
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+      },
     ],
   },
 
